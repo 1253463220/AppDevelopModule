42baa9503f077b770d57a895aaf6e568d1836f91
王立 <1253463220@qq.com>
2021-08-06 13:54:51 +0800

    完善WLTextView

--- AppDevelopModule/Core/GeneralView/WLTextView/WLTextView.swift
+++ AppDevelopModule/Core/GeneralView/WLTextView/WLTextView.swift
@@ -16,6 +16,7 @@ import UIKit
     @objc var maxHeight : CGFloat = -1
     @objc var heightChangeBlock : ((_ textView:WLTextView,_ height:CGFloat)->Void)?
     @objc var textChangedBlock : ((_ textView:WLTextView,_ text:String)->Void)?
+    @objc var handleEmoji = false//是否处理表情等特殊字符
     
     private var lastText : String?//最后一次处理后的文字
     override init(frame: CGRect, textContainer: NSTextContainer?) {
@@ -45,6 +46,7 @@ import UIKit
     }
     
     private func doLimitTextCount(textView:UITextView){
+        guard self.limitTextCount > 0 else {return}
         let textStr = textView.text ?? ""
         let mode = UITextInputMode.activeInputModes.first
         let lang = mode?.primaryLanguage
@@ -52,16 +54,46 @@ import UIKit
             let range = textView.markedTextRange ?? UITextRange.init()
             let position = textView.position(from: range.start, offset: 0)
             if position == nil {
+                if handleEmoji{
+                    handleEmojiForLimitCount()
+                }else{
+                    if textStr.count > limitTextCount {
+                        self.text = textStr.substring(toIndex: limitTextCount)
+                    }
+                }
+            }
+        }else{
+            if handleEmoji {
+                handleEmojiForLimitCount()
+            }else{
                 if textStr.count > limitTextCount {
                     self.text = textStr.substring(toIndex: limitTextCount)
                 }
             }
-        }else{
-            if textStr.count > limitTextCount {
-                self.text = textStr.substring(toIndex: limitTextCount)
+        }
+    }
+    
+    private func handleEmojiForLimitCount(){
+        guard self.limitTextCount > 0 else {return}
+        let textStr = self.text ?? ""
+        if (textStr as NSString).length > limitTextCount {
+            if textStr.count == (textStr as NSString).length {//没有表情等特殊字符，或含有表情字节长度为1
+                self.text = (textStr as NSString).substring(to: limitTextCount)
+            }else{
+                let tempStr = (textStr as NSString).substring(to: limitTextCount)
+                if let character = tempStr.last {
+                    if self.text.contains(character) {//最后一个字符没有被截断
+                        self.text = (textStr as NSString).substring(to: limitTextCount)
+                    }else{
+                        let nsStr = String.init(character) as NSString
+                        let reduceCount = max(0, limitTextCount-nsStr.length)//nsStr.length:character这个字符的长度
+                        self.text = (textStr as NSString).substring(to: reduceCount)
+                    }
+                }
             }
         }
     }
+    
     
     @objc func fitHeight()->CGFloat{
         if minHeight == -1 || maxHeight == -1 {
@@ -90,6 +122,16 @@ import UIKit
                 }
                 self.frame = CGRect.init(x: self.frame.origin.x, y: self.frame.origin.y, width: self.frame.size.width, height: finalH)
             }
+        }
+    }
+    
+    ///剩余可用字数
+    @objc func leftTextCount()->Int{
+        guard self.limitTextCount != -1 else {return 0}
+        if self.handleEmoji {
+            return self.limitTextCount - (self.text as NSString).length
+        }else{
+            return self.limitTextCount - self.text.count
         }
     }
     
