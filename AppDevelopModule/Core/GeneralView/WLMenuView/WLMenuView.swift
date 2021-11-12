ead330f615fc71902a1256d97426ec1538529562
王立 <w1253463220@163.com>
2021-11-12 13:16:26 +0800

    WLMenuView

--- AppDevelopModule/Core/GeneralView/WLMenuView/WLMenuView.swift
+++ AppDevelopModule/Core/GeneralView/WLMenuView/WLMenuView.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class WLMenuView: WLScroContentView {
     
-    private var itemArr = [WLMenuItemView]()
-    var selectedIndex : Int?
-    var indexWillChanged : ((Int,WLMenuItemView)->Bool)?
-    var indexDidChanged : ((Int,WLMenuItemView)->Void)?
+    typealias IndicatorUIConfigAct = (_ index:Int,_ itemV:WLMenuItemView)->UIView
+    private(set) var itemArr = [WLMenuItemView]()
+    private(set) var selectedIndex : Int?
+    var indexWillChanged : ((_ index:Int,_ itemV:WLMenuItemView)->Bool)?
+    var indexDidChanged : ((_ index:Int,_ itemV:WLMenuItemView)->Void)?
+    private var indicatorV : UIView?//指示view
     private var selectedItemV : WLMenuItemView?
+    private var indicatorConfig : IndicatorUIConfigAct?
     // MARK: 生命周期
     init(itemArr:[WLMenuItemView],selectedIndex:Int? = 0,itemSpace:CGFloat = 0) {
         super.init(contentEdge: .zero, itemSpace: itemSpace)
@@ -36,33 +39,58 @@ class WLMenuView: WLScroContentView {
             }else{
                 itemV.normalUIConfig(itemV)
             }
-            itemV.addTapAction {[weak self,weak itemV] ges in
-                guard let sItemV = itemV else {return}
+            itemV.addTapAction {[weak self] ges in
                 guard let self = self else {return}
-                self.tap(index: index, itemV: sItemV)
+                self.tap(index: index)
             }
         }
         add(views: itemArr)
     }
     
+    func configIndicator(configAct:@escaping IndicatorUIConfigAct){
+        guard let tSelectIndex = self.selectedIndex else {return}
+        guard let tSelectItemV = self.selectedItemV else {return}
+        self.indicatorV = configAct(tSelectIndex,tSelectItemV)
+        self.indicatorConfig = configAct
+        self.addSubview(self.indicatorV!)
+    }
+    
     // MARK: 数据
     
     // MARK: 事件
-    @objc private func tap(index:Int,itemV:WLMenuItemView){
-        guard index != selectedIndex else {return}
-        let shouldContinue = self.indexWillChanged?(index, itemV) ?? true
-        if shouldContinue {
-            if self.selectedItemV != nil {
-                self.selectedItemV?.normalUIConfig(self.selectedItemV!)
-            }
-            self.selectedIndex = index
-            self.selectedItemV = itemV
-            self.indexDidChanged?(index,itemV)
-            self.selectedItemV?.selectUIConfig(itemV)
-        }
+    @objc private func tap(index:Int){
+        gotoIndex(index)
     }
     
     // MARK: 逻辑
+    func gotoIndex(_ index:Int,animated:Bool = true){
+        guard index != selectedIndex else {return}
+        guard index >= 0,index < itemArr.count else {return}
+        
+        let act : (()->Void) = {
+            let itemV = self.itemArr[index]
+            let shouldContinue = self.indexWillChanged?(index, itemV) ?? true
+            if shouldContinue {
+                if self.selectedItemV != nil {
+                    self.selectedItemV?.normalUIConfig(self.selectedItemV!)
+                }
+                self.selectedIndex = index
+                self.selectedItemV = itemV
+                self.indexDidChanged?(index,itemV)
+                self.selectedItemV?.selectUIConfig(itemV)
+                _ = self.indicatorConfig?(index,itemV)
+            }
+        }
+        
+        if animated {
+            UIView.animate(withDuration: 0.2) {
+                act()
+            }
+        }else{
+            act()
+        }
+        
+    }
     
     // MARK: 代理
     
@@ -87,4 +115,91 @@ class WLMenuItemView: UIButton {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+}
+
+
+
+
+///复选视图
+class WLMultipleChooseView: WLScroContentView {
+    private(set) var itemArr = [WLMenuItemView]()
+    private(set) var selectedIndexs = [Int]()
+    var indexWillSelected : ((_ currentSelectIndex:Int,_ currentSelectItemV:WLMenuItemView)->Bool)?
+    var indexEndSelected : ((_ allSelectIndexs:[Int],_ allSelectItemVs:[WLMenuItemView])->Void)?
+    
+    // MARK: 生命周期
+    init(itemArr:[WLMenuItemView],selectedIndexs:[Int] = [],itemSpace:CGFloat = 0) {
+        super.init(contentEdge: .zero, itemSpace: itemSpace)
+        self.itemArr = itemArr
+        self.selectedIndexs = selectedIndexs
+        configUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: 界面
+    private func configUI() {
+        for (index,itemV) in itemArr.enumerated() {
+            if selectedIndexs.contains(index) {
+                itemV.selectUIConfig(itemV)
+            }else{
+                itemV.normalUIConfig(itemV)
+            }
+            itemV.addTapAction {[weak self] ges in
+                guard let self = self else {return}
+                self.tap(index: index)
+            }
+        }
+        add(views: itemArr)
+    }
+    // MARK: 数据
+    
+    // MARK: 事件
+    @objc private func tap(index:Int){
+        if self.selectedIndexs.contains(index) {
+            var arr = selectedIndexs
+            arr.removeAll(where: {$0 == index})
+            changeSelectedIndexs(arr)
+        }else{
+            changeSelectedIndexs(selectedIndexs+[index])
+        }
+    }
+    
+    // MARK: 逻辑
+    func changeSelectedIndexs(_ indexs:[Int],animated:Bool = true){
+        let act : (()->Void) = {
+            for index in self.selectedIndexs {
+                let itemV = self.itemArr[index]
+                itemV.normalUIConfig(itemV)
+            }
+            var allSelectItemArr = [WLMenuItemView]()
+            for index in indexs {
+                let itemV = self.itemArr[index]
+                allSelectItemArr.append(itemV)
+                let shouldContinue = self.indexWillSelected?(index, itemV) ?? true
+                var finalIndexArr = [Int]()
+                if shouldContinue {
+                    finalIndexArr.append(index)
+                    itemV.selectUIConfig(itemV)
+                }
+            }
+            self.selectedIndexs = indexs
+            self.indexEndSelected?(indexs,allSelectItemArr)
+        }
+        
+        if animated {
+            UIView.animate(withDuration: 0.2) {
+                act()
+            }
+        }else{
+            act()
+        }
+        
+    }
+    
+    // MARK: 代理
+    
+    // MARK: lazy
 }
